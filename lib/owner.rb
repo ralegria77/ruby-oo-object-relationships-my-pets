b31a087409a5bed96de91bbbe3af8dd98284a065
rebec <ralegria77@gmail.com>
2020-11-14 21:32:54 -0800

    commit tree f21eaa676dacc7fae0df5ca4e8e159dbb44fc104

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,66 @@
 class Owner
-  # code goes here
+
+attr_reader :name, :species
+@@all = []
+
+def initialize(name)
+  @name = name
+  @species = "human"
+  @@all << self
+end
+
+def say_species
+  "I am a #{self.species}."
+end
+
+def self.all
+  @@all
+end
+
+def self.count
+  @@all.count #.length would also work
+end
+
+def self.reset_all
+  @@all.clear
+end
+
+def cats
+  Cat.all.select {|cat| cat.owner == self} #all the cats that belong to owner
+end
+
+def dogs
+  Dog.all.select {|dog| dog.owner == self} #returns all the dogs that belong to owner
+end
+
+def buy_cat(name)
+  Cat.new(name, self)
+end
+
+def buy_dog(name)
+  Dog.new(name, self)
+end
+
+def walk_dogs
+  dogs.select {|dog| dog.mood = "happy"}
+end
+
+def feed_cats
+  cats.select {|cat| cat.mood = "happy"}
+end
+
+def sell_pets
+  pets = self.dogs + self.cats
+  pets.map do |pet| 
+  pet.mood = "nervous"
+  pet.owner = nil
+  end
+end
+
+def list_pets
+  "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+end
+
+
+
 end
